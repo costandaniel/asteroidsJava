b5fd9ce96c759e80d3fff791f41659daeb4aecd9
Costan Daniel <danielcostan99@gmail.com>
2023-10-04 22:13:47 +0300

    abstracting character class

--- src/main/java/com/example/asteroids/AsteroidsApplication.java
+++ src/main/java/com/example/asteroids/AsteroidsApplication.java
@@ -1,25 +1,56 @@
 package com.example.asteroids;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AsteroidsApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        Map<KeyCode, Boolean> pressedKeys = new HashMap<>();
         Pane pane = new Pane();
         pane.setPrefSize(600, 400);
-        Polygon parallelogram = new Polygon(0, 0, 100, 0, 100, 50, 0, 50);
-        parallelogram.setTranslateX(100);
-        parallelogram.setTranslateY(20);
-        pane.getChildren().add(parallelogram);
+        Ship ship = new Ship(150, 100);
+
+
+        pane.getChildren().add(ship.getCharacter());
 
         Scene scene = new Scene(pane);
+        scene.setOnKeyPressed(event -> {
+            pressedKeys.put(event.getCode(), Boolean.TRUE);
+        });
+
+        scene.setOnKeyReleased(event -> {
+            pressedKeys.put(event.getCode(), Boolean.FALSE);
+        });
         stage.setTitle("Asteroids!");
         stage.setScene(scene);
         stage.show();
+        new AnimationTimer() {
+
+            @Override
+            public void handle(long now) {
+                if (pressedKeys.getOrDefault(KeyCode.LEFT, false)) {
+                    ship.turnLeft();
+                }
+
+                if (pressedKeys.getOrDefault(KeyCode.RIGHT, false)) {
+                    ship.turnRight();
+                }
+                if(pressedKeys.getOrDefault(KeyCode.UP, false)) {
+                    ship.accelerate();
+                }
+                ship.move();
+            }
+
+        }.start();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/asteroids/Character.java
@@ -0,0 +1,9 @@
+package com.example.asteroids;
+
+import javafx.geometry.Point2D;
+import javafx.scene.shape.Polygon;
+
+public abstract class Character {
+    private Polygon character;
+    private Point2D movement;
+}
