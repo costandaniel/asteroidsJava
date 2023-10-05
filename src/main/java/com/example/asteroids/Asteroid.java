2d32f46e63f169a9d302e64732e2b3ee02a8ed86
Costan Daniel <danielcostan99@gmail.com>
2023-10-05 11:16:31 +0300

    Varying asteroids

---
+++ src/main/java/com/example/asteroids/Asteroid.java
@@ -0,0 +1,11 @@
+package com.example.asteroids;
+
+import javafx.scene.shape.Polygon;
+
+public class Asteroid extends Character {
+
+    public Asteroid(int x, int y) {
+        super(new PolygonFactory().createPolygon(), x, y);
+    }
+
+}
--- src/main/java/com/example/asteroids/AsteroidsApplication.java
+++ src/main/java/com/example/asteroids/AsteroidsApplication.java
@@ -5,11 +5,9 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class AsteroidsApplication extends Application {
     @Override
@@ -18,9 +16,16 @@ public class AsteroidsApplication extends Application {
         Pane pane = new Pane();
         pane.setPrefSize(600, 400);
         Ship ship = new Ship(150, 100);
+        List<Asteroid> asteroids = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Random rnd = new Random();
+            Asteroid asteroid = new Asteroid(rnd.nextInt(100), rnd.nextInt(100));
+            asteroids.add(asteroid);
+        }
 
 
         pane.getChildren().add(ship.getCharacter());
+        asteroids.forEach(asteroid -> pane.getChildren().add(asteroid.getCharacter()));
 
         Scene scene = new Scene(pane);
         scene.setOnKeyPressed(event -> {
@@ -44,10 +49,17 @@ public class AsteroidsApplication extends Application {
                 if (pressedKeys.getOrDefault(KeyCode.RIGHT, false)) {
                     ship.turnRight();
                 }
-                if(pressedKeys.getOrDefault(KeyCode.UP, false)) {
+                if (pressedKeys.getOrDefault(KeyCode.UP, false)) {
                     ship.accelerate();
                 }
+
                 ship.move();
+                asteroids.forEach(asteroid -> asteroid.move());
+                asteroids.forEach(asteroid -> {
+                    if (ship.collide(asteroid)) {
+                        stop();
+                    }
+                });
             }
 
         }.start();
