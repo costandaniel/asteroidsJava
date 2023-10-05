20d5207974df8e809ddd21973cf62c174840b0f5
Costan Daniel <danielcostan99@gmail.com>
2023-10-05 11:47:31 +0300

    finished

--- src/main/java/com/example/asteroids/Ship.java
+++ src/main/java/com/example/asteroids/Ship.java
@@ -4,9 +4,6 @@ import javafx.geometry.Point2D;
 import javafx.scene.shape.Polygon;
 
 public class Ship extends Character {
-    private Polygon character;
-    private Point2D movement;
-
     public Ship(int x, int y) {
         super(new Polygon(-5, -5, 10, 0, -5, 5), x, y);
     }
